bc4feaf2c177ed54d378b129593c2e376f74c695
Adrian Simmons <adrian@perlucida.com>
2018-11-08 13:35:04 +0000

    Switch to stage 2 for postcss-preset-env, dont include nesting

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,9 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-preset-env')({
-      /* use stage 3 features + css nesting rules */
-      stage: 3,
-      features: {
-        'nesting-rules': true
-      }
+      /* use stage 2 rules */
+      stage: 2,
+      preserve: false
     }),
     require('autoprefixer'),
     require('postcss-reporter')
